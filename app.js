bcaf8d1c459c0cb106ce85c47092efb076071fbc
scottymatthewman <127428296+scottymatthewman@users.noreply.github.com>
2025-05-13 16:44:28 -0400

    updating expo-sqlite, editing app.js file

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 import { SQLiteProvider } from 'expo-sqlite';
+import React from 'react';
 import NewSongForm from './app/(tabs)/newSongForm';
 
 export default function App() {
-    return (
-      <SQLiteProvider 
-          databaseName="userDatabase.db"
-          onInit={async (db) => {
-              await db.execAsync(`
-                  CREATE TABLE IF NOT EXISTS files (
-                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      title TEXT NOT NULL,
-                      content TEXT NOT NULL
-                  )
-              `);
-          }}
-          options={{useNewConnection: false}}
-      >
-        <NewSongForm />
-      </SQLiteProvider>
-    );
-  }
+  return (
+    <SQLiteProvider 
+        databaseName="userDatabase.db"
+        onInit={async (db) => {
+            await db.execAsync(`
+                CREATE TABLE IF NOT EXISTS files (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    content TEXT NOT NULL
+                )
+            `);
+        }}
+        options={{useNewConnection: false}}
+    >
+      <NewSongForm />
+    </SQLiteProvider>
+  );
+}
