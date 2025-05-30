58c5a4cd9241bf238c91f2fa3c847b79df1694e7
scottymatthewman <127428296+scottymatthewman@users.noreply.github.com>
2025-05-30 18:17:59 -0400

    added song details dropdown to newSong

--- app/newSong.tsx
+++ app/newSong.tsx
@@ -1,3 +1,4 @@
+import DropdownOutlineDownIcon from '@/components/icons/DropdownOutlineDownIcon';
 import DOMComponent from '@/components/RichTextEditor';
 import { MusicalKey } from '@/constants/musicalKeys';
 import { useLocalSearchParams, useRouter } from 'expo-router';
@@ -21,6 +22,7 @@ const NewSong = () => {
     const router = useRouter();
     const [showActions, setShowActions] = useState(false);
     const [selectedKey, setSelectedKey] = useState<MusicalKey | null>(null);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const { theme: currentTheme } = useTheme();
     const colorPalette = currentTheme === 'dark' ? theme.colors.dark : theme.colors.light;
 
@@ -94,13 +96,39 @@ const NewSong = () => {
                     <Text className={currentTheme === 'dark' ? 'text-dark-text-body' : 'text-light-text-body'} style={{ fontSize: 18, fontWeight: '600' }}>Save</Text>
                 </TouchableOpacity>
             </View>
-            <TextInput 
-                className={`placeholder:${currentTheme === 'dark' ? 'text-dark-text-placeholder' : 'text-light-text-placeholder'} text-3xl font-semibold pt-4 pl-6 pr-6 pb-3 ${currentTheme === 'dark' ? 'text-dark-text-header' : 'text-light-text-header'}`}
-                placeholder="Untitled"
-                value={title} 
-                onChangeText={setTitle}
-            />
-            <ScrollView className="pl-6 pr-6">
+            <View className="flex-row justify-between items-center pt-4 pl-6 pr-4 pb-1">
+                <TextInput 
+                    className={`placeholder:${currentTheme === 'dark' ? 'text-dark-text-placeholder' : 'text-light-text-placeholder'} text-3xl font-semibold ${currentTheme === 'dark' ? 'text-dark-text-header' : 'text-light-text-header'}`}
+                    placeholder="Untitled"
+                    value={title} 
+                    onChangeText={setTitle}
+                />
+                <TouchableOpacity onPress={() => setIsDropdownOpen(!isDropdownOpen)}>
+                    <View style={{ transform: [{ rotate: isDropdownOpen ? '180deg' : '0deg' }] }}>
+                        <DropdownOutlineDownIcon width={28} height={28} fill={currentTheme === 'dark' ? theme.colors.dark.textPlaceholder : theme.colors.light.textPlaceholder} />
+                    </View>
+                </TouchableOpacity>
+            </View>
+            {isDropdownOpen && (
+                <View className={`mt-2 pt-3 ${currentTheme === 'dark' ? 'bg-dark-surface2' : 'bg-light-surface1'} border-y ${currentTheme === 'dark' ? 'border-dark-border' : 'border-light-border'}`}>
+                    <View className="px-6 pb-4 flex-row items-center justify-between border-b" style={{ borderColor: currentTheme === 'dark' ? theme.colors.dark.border : theme.colors.light.border }}>
+                        <Text className={currentTheme === 'dark' ? 'text-dark-text-placeholder' : 'text-light-text-placeholder'}>Attachments</Text>
+                        <Text className={currentTheme === 'dark' ? 'text-dark-text-body' : 'text-light-text-body'}>0</Text>
+                    </View>
+                    <View className="px-6 flex-row justify-stretch items-center gap-4">
+                        <View className="flex-row py-3 grow items-center justify-between">
+                            <Text className={currentTheme === 'dark' ? 'text-dark-text-placeholder' : 'text-light-text-placeholder'}>Key</Text>
+                            <Text className={currentTheme === 'dark' ? 'text-dark-text-body' : 'text-light-text-body'}>{selectedKey || 'None'}</Text>
+                        </View>
+                        <View className="w-[1px] h-full" style={{ backgroundColor: currentTheme === 'dark' ? theme.colors.dark.border : theme.colors.light.border }}></View>
+                        <View className="flex-row py-4 grow items-center justify-between">
+                            <Text className={currentTheme === 'dark' ? 'text-dark-text-placeholder' : 'text-light-text-placeholder'}>BPM</Text>
+                            <Text className={currentTheme === 'dark' ? 'text-dark-text-body' : 'text-light-text-body'}>120</Text>
+                        </View>
+                    </View>
+                </View>
+            )}
+            <ScrollView className="px-6 pt-1">
                 <TextInput 
                     className={`text-xl/9 font-normal ${currentTheme === 'dark' ? 'text-dark-text placeholder:text-dark-text-placeholder' : 'text-light-text placeholder:text-light-text-placeholder'} ${currentTheme === 'dark' ? 'text-dark-text-body' : 'text-light-text-body'}`}
                     placeholder="I heard there was a secret chord..."
